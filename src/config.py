0185aed0fa2f99086cb88f73c31ca6c6708ce6d1
tlgb <tlgbkakaka@gmail.com>
2026-02-25 14:32:31 +0700

    removed unused configs

--- src/config.py
+++ src/config.py
@@ -2,11 +2,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SOURCE = os.path.join(BASE_DIR, '../data_source/products-0-1500.csv')
+SOURCE = os.path.join(BASE_DIR, '../data_source/products-0-200000.csv')
 OUTPUT_DIR = os.path.join(BASE_DIR, '../processed_data')
-CONCURRENCY_LIMIT = 30
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/114.0.0.0'
-}
+CONCURRENCY_LIMIT = 40
+RATE_LIMIT = 60
+RETRIES = 3
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
